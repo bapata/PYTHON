67c93801175ea79e0a7f4669992c10eefcd890c1
anand <bapata@ymail.com>
2013-11-25 20:01:25 -0800

    improvements

--- add.py
+++ add.py
@@ -1,9 +1,39 @@
 #!/usr/bin/python
 
+import io, sys
+
 # Script to test main invocation
-def main():
+def print_arguments(argv):
+    # Count
+    argc = len(argv)
+
+    # Walk all arguments and print it
+    for ii in range(argc):
+        print "argv[" + str(ii) + "] is " + argv[ii]
+
+# Input: List of integers
+# Output : Sum
+def sum_of_args(list):
+    list_count=len(list)
+
+    sum=0
+    # Walk list and calculate sum
+    for ii in range(list_count):
+        sum+=list[ii]
+
+    return sum
+
+    
+
+
+def main(argv):
     print "I am in main"
+
+    print_arguments(argv)
+    int_list=[int(x) for x in argv[1:]]
+    sum=sum_of_args(int_list)
+    print "Sum : " + str(sum)
 
 
 if (__name__ == "__main__"):
-    main()
+    main(sys.argv)
