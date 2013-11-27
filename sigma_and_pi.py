a9102c536ee69033b06d426f0741a1ccb1415e57
anand <bapata@ymail.com>
2013-11-26 20:35:35 -0800

    improvements

--- sigma_and_pi.py
+++ sigma_and_pi.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python
 
+#
+## Script to calculate summation and factorial of N
+# ./sigma_and_pi.py 10
+# Summation of 10 is 55
+# Factorial of 10 is 3628800
+#
+
 import sys,os
 
-def sum_of_numbers(n):
-    sum=0
-    for x in range(1,n+1):
-        sum+=x
-    return sum
+def sum_upto_n(n):
+    return 0 if (n<0) else n + sum_upto_n(n-1)
 
-def product_of_numbers(n):
-    return 1 if (n<=0) else n * product_of_numbers(n - 1)
+def product_upto_n(n):
+    return 1 if (n<=0) else n * product_upto_n(n - 1)
 
 #
 ## main starts here
@@ -24,13 +28,15 @@ def usage():
 def main(argv):
     argc=len(argv)
 
+    # ARGV[1] = program name
+    # ARGV[2] = N
     if(argc!=2):
         usage()
 
     number=int(argv[1])
 
-    print "Sum of 1 to " + str(number) + " is " + str(sum_of_numbers(number))
-    print "\n1x2x3x.." + str(number) + " is " + str(product_of_numbers(number))
+    print "Summation of " + str(number) + " is " + str(sum_upto_n(number))
+    print "Factorial of " + str(number) + " is " + str(product_upto_n(number))
 
 
 if __name__ == "__main__":
