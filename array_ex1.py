398dde1c04d84b193051bcac434884e5253d42a4
anand <bapata@ymail.com>
2013-11-26 21:00:58 -0800

    array examples

---
+++ array_ex1.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+
+def main():
+    snacks=['idly','dosa','vada','bisibelebhath','chowchowbhat']
+    lunch_items=['chapati','rice','pulao']
+    sweets=['mysorepaak','chiroti','kazubarfi','rasmalai']
+
+    snacks_cap=[s.capitalize() for s in snacks]
+    print "==Snack List:==" 
+    print snacks_cap
+    print ""
+
+    print "==Lunch List:==" 
+    lunch_up=[l.upper() for l in lunch_items]
+    print lunch_up
+    print ""
+
+    # I didn't know that + works for list as well
+    all_food=snacks + lunch_items + sweets
+    print "==Everything:==" 
+    print all_food
+    print ""
+
+    # Reverse act in-place
+    all_food.reverse()
+    print all_food
+    # Give me idly and keep remaining list
+    print "I love to eat " + all_food.pop() + " as breakfast"
+    print all_food.pop() + " is my favorite "
+
+    # I keep remaining list
+    print all_food
+
+if __name__ == "__main__":
+    main()
