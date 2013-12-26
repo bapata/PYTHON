259c384e30d66d2d24eed12798befbd0469af65d
anand <bapata@ymail.com>
2013-12-25 20:39:51 -0800

    set examples

---
+++ set_ex1.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+from sets import Set
+
+a=['one','two','four']
+b=['one','three','four','five']
+
+setA=Set(a)
+setB=Set(b)
+
+print "SET A is : " 
+print setA
+
+print "SET B is : " 
+print setB
+
+print "Union of A and B is :"
+# setAB=setA | setB
+setAB=setA.union(setB)
+print setAB
+
+print "Intersection of A and B is :"
+# setAB=setA & setB
+setAB=setA.intersection(setB)
+print setAB
+
+print "Difference : A - B is :"
+# setAB=setA - setB
+setAB=setA.difference(setB)
+print setAB
+
+print "Difference : B - A is :"
+# setAB=SetB - setA
+setAB=setB.difference(setA)
+print setAB
