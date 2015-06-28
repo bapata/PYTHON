7e94805632bbd4e24086a56958bbdff7a36dadce
anand <bapata@ymail.com>
2015-06-28 16:39:18 -0700

    initial addition

---
+++ odd_or_even1.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+
+def print_even_list(list):
+  print "EVEN:"
+  for ii in list:
+    if ii%2 == 0:
+      print ii
+
+def print_odd_list(list):
+  print "ODD:"
+  for ii in list:
+    if ii%2 != 0:
+      print ii
+
+def main():
+  list=range(1,11)  
+  print_odd_list(list)
+  print_even_list(list)
+
+
+
+if __name__ == "__main__":
+  main()
+
+
