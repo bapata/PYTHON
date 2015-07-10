50fb5ac47ab6fa6b8b1df6fe87121db78469c130
anand <bapata@ymail.com>
2015-07-09 19:34:06 -0700

    added

--- yml2dict.py
+++ yml2dict.py
@@ -1,5 +1,11 @@
+#!/usr/bin/env python
+
 import yaml
-f = open('tree.yaml')
+import sys
+
+# <script> <yaml-file>
+
+f = open(sys.argv[1])
 # use safe_load instead load
 dataMap = yaml.safe_load(f)
 f.close()
