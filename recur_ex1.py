5ea1980d831d454597471f1d2fa4ce5bb6b5743b
bapata <bapata@ymail.com>
2018-12-29 23:42:00 -0800

    few more functions

--- recur_ex1.py
+++ recur_ex1.py
@@ -23,12 +23,43 @@ def int_power(m,n):
   else:
     return m * int_power(m,n-1)
 
+## One more version
+def my_pow(x,y):
+  if x==0:
+    return 0
+  elif x==1:
+    return 1
+  elif y==1:
+    return x
+  else:
+    return x*my_pow(x,y-1)
+  
 def factorial(n):
   if (n==0):
     return 1
   else:
     return n * factorial(n-1)
 
+## Sum and prod of contents in a list
+# Sum of list
+def sum_list(mylist):
+  if len(mylist)==0:
+    return 0
+  elif len(mylist)==1:
+    return mylist[0]
+  else:
+    return mylist[0]+sum_list(mylist[1:])
+
+# Prod of a list
+def prod_list(mylist):
+  if len(mylist)==0:
+    return 0
+  elif len(mylist)==1:
+    return mylist[0]
+  else:
+    return mylist[0]*sum_list(mylist[1:])
+  
+  
 def main():
   a=[1,2,3]
   print add_contents(a) # Expect 6
