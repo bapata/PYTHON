49003b8b03a84f19dbd0010017b86116064fd226
bapata <bapata@ymail.com>
2022-01-16 11:13:04 -0800

    Create sentence_reverse.py

---
+++ sentence_reverse.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# Given a string s, reverse the words.
+# Note: You may assume that there are no leading or trailing whitespaces and each word within s is only separated by a single whitespace.
+# s = "The Daily Byte", return "Byte Daily The".
+
+s="my sentence to reverse"
+print(list(reversed(s.split()))
