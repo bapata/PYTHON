e7710090cd4ea6d1e22687b9df3e9cb7316d0082
bapata <bapata@ymail.com>
2024-12-25 17:12:35 -0800

    Update move_zero_to_end.py

--- move_zero_to_end.py
+++ move_zero_to_end.py
@@ -1,13 +1,21 @@
 #!/usr/bin/env python3
+# Move x to the end
+# x is in the list
+def move_x_to_end(list,x):
+  for ii in list:
+    if ii==x:
+      list.remove(ii)
+      list.append(ii)
+  return list
 
-## Given this list
-list = [1,0,2,0,3,0,4,5,6]
-### Write a function to modify the above list such that
-## all zeros are at the end
+# Given this list
+list = [1,0,2,0,3,0,1,4,5,6]
 
-for ii in list:
-  if ii==0:
-    list.remove(ii)
-    list.append(ii)
+# Write a function to modify the above list such that
+# all x are at the end
 
-print(list)
+list1=move_x_to_end(list,0)
+print(list1)
+
+list2=move_x_to_end(list,1)
+print(list2)
